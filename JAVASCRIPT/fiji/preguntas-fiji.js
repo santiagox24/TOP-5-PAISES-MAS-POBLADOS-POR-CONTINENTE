1a9c7bedc3b2066ee0c2a1735c9ce535cfcd38bc
santiagox24 <santiagosuarezherreros@gmail.com>
2024-12-07 10:51:37 -0500

    Se termino la pagina web de Fiyi.

---
+++ JAVASCRIPT/fiji/preguntas-fiji.js
@@ -0,0 +1,266 @@
+let preguntas = [
+    {
+      enunciado_pregunta: "¿Cuál es la capital de Fiyi?",
+      opciones: [
+        { textoOpcion: "Nadi", respuestaCorrecta: false },
+        { textoOpcion: "Suva", respuestaCorrecta: true },
+        { textoOpcion: "Lautoka", respuestaCorrecta: false },
+        { textoOpcion: "Savusavu", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es el deporte más popular en Fiyi?",
+      opciones: [
+        { textoOpcion: "Críquet", respuestaCorrecta: false },
+        { textoOpcion: "Fútbol", respuestaCorrecta: false },
+        { textoOpcion: "Rugby", respuestaCorrecta: true },
+        { textoOpcion: "Baloncesto", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué bebida tradicional se consume en Fiyi como parte de sus ceremonias?",
+      opciones: [
+        { textoOpcion: "Kava", respuestaCorrecta: true },
+        { textoOpcion: "Vino de palma", respuestaCorrecta: false },
+        { textoOpcion: "Cerveza", respuestaCorrecta: false },
+        { textoOpcion: "Coca de mate", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué isla principal de Fiyi alberga la capital Suva?",
+      opciones: [
+        { textoOpcion: "Vanua Levu", respuestaCorrecta: false },
+        { textoOpcion: "Viti Levu", respuestaCorrecta: true },
+        { textoOpcion: "Taveuni", respuestaCorrecta: false },
+        { textoOpcion: "Kadavu", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuántas islas aproximadamente componen Fiyi?",
+      opciones: [
+        { textoOpcion: "50", respuestaCorrecta: false },
+        { textoOpcion: "100", respuestaCorrecta: false },
+        { textoOpcion: "300", respuestaCorrecta: true },
+        { textoOpcion: "500", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es el arrecife de coral más famoso de Fiyi?",
+      opciones: [
+        { textoOpcion: "Gran Barrera de Coral", respuestaCorrecta: false },
+        { textoOpcion: "Arrecife de Ningaloo", respuestaCorrecta: false },
+        { textoOpcion: "Arrecife de Coral de Great Astrolabe", respuestaCorrecta: true },
+        { textoOpcion: "Arrecife de Aldabra", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es la economía principal de Fiyi?",
+      opciones: [
+        { textoOpcion: "Minería", respuestaCorrecta: false },
+        { textoOpcion: "Turismo", respuestaCorrecta: true },
+        { textoOpcion: "Manufactura", respuestaCorrecta: false },
+        { textoOpcion: "Tecnología", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué grupo de islas en Fiyi es conocido por su belleza y fue escenario de la película 'Castaway'?",
+      opciones: [
+        { textoOpcion: "Islas Mamanuca", respuestaCorrecta: true },
+        { textoOpcion: "Islas Yasawa", respuestaCorrecta: false },
+        { textoOpcion: "Islas Lau", respuestaCorrecta: false },
+        { textoOpcion: "Islas Lomaiviti", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es el idioma oficial de Fiyi, además del inglés?",
+      opciones: [
+        { textoOpcion: "Hindi", respuestaCorrecta: true },
+        { textoOpcion: "Francés", respuestaCorrecta: false },
+        { textoOpcion: "Chino", respuestaCorrecta: false },
+        { textoOpcion: "Portugués", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué famosa atracción en Fiyi es conocida por su colección de orquídeas?",
+      opciones: [
+        { textoOpcion: "Jardín del Gigante Dormido", respuestaCorrecta: true },
+        { textoOpcion: "Parque Nacional Bouma", respuestaCorrecta: false },
+        { textoOpcion: "Playa Natadola", respuestaCorrecta: false },
+        { textoOpcion: "Isla de Monuriki", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué evento deportivo internacional ganó Fiyi por primera vez en 2016?",
+      opciones: [
+        { textoOpcion: "Copa del Mundo de Rugby", respuestaCorrecta: false },
+        { textoOpcion: "Medalla de Oro en Rugby a Siete", respuestaCorrecta: true },
+        { textoOpcion: "Copa Asiática de Fútbol", respuestaCorrecta: false },
+        { textoOpcion: "Torneo de Golf Masters", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué islas de Fiyi son conocidas por sus playas vírgenes y opciones de alojamiento económico?",
+      opciones: [
+        { textoOpcion: "Islas Mamanuca", respuestaCorrecta: false },
+        { textoOpcion: "Islas Yasawa", respuestaCorrecta: true },
+        { textoOpcion: "Islas Lomaiviti", respuestaCorrecta: false },
+        { textoOpcion: "Islas Kadavu", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es el pico más alto de Fiyi?",
+      opciones: [
+        { textoOpcion: "Monte Fuji", respuestaCorrecta: false },
+        { textoOpcion: "Mount Victoria", respuestaCorrecta: false },
+        { textoOpcion: "Monte Tomanivi", respuestaCorrecta: true },
+        { textoOpcion: "Monte Everest", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué isla de Fiyi es conocida por sus densos bosques y la observación de aves?",
+      opciones: [
+        { textoOpcion: "Vanua Levu", respuestaCorrecta: false },
+        { textoOpcion: "Taveuni", respuestaCorrecta: true },
+        { textoOpcion: "Viti Levu", respuestaCorrecta: false },
+        { textoOpcion: "Kadavu", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué recurso natural es fundamental para la economía de Fiyi?",
+      opciones: [
+        { textoOpcion: "Petróleo", respuestaCorrecta: false },
+        { textoOpcion: "Oro", respuestaCorrecta: false },
+        { textoOpcion: "Azúcar", respuestaCorrecta: true },
+        { textoOpcion: "Hierro", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es la religión predominante en Fiyi?",
+      opciones: [
+        { textoOpcion: "Cristianismo", respuestaCorrecta: true },
+        { textoOpcion: "Islam", respuestaCorrecta: false },
+        { textoOpcion: "Budismo", respuestaCorrecta: false },
+        { textoOpcion: "Hinduismo", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es la zona horaria de Fiyi?",
+      opciones: [
+        { textoOpcion: "GMT+5", respuestaCorrecta: false },
+        { textoOpcion: "GMT+12", respuestaCorrecta: true },
+        { textoOpcion: "GMT+8", respuestaCorrecta: false },
+        { textoOpcion: "GMT+10", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué animal marino es comúnmente visto en las aguas de Fiyi?",
+      opciones: [
+        { textoOpcion: "Delfín", respuestaCorrecta: false },
+        { textoOpcion: "Tortuga marina", respuestaCorrecta: true },
+        { textoOpcion: "Ballena azul", respuestaCorrecta: false },
+        { textoOpcion: "León marino", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Cuál es el festival hindú más importante celebrado en Fiyi?",
+      opciones: [
+        { textoOpcion: "Holi", respuestaCorrecta: false },
+        { textoOpcion: "Diwali", respuestaCorrecta: true },
+        { textoOpcion: "Eid al-Fitr", respuestaCorrecta: false },
+        { textoOpcion: "Navratri", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué isla de Fiyi es conocida como la 'Capital de los Arrecifes'?",
+      opciones: [
+        { textoOpcion: "Kadavu", respuestaCorrecta: true },
+        { textoOpcion: "Taveuni", respuestaCorrecta: false },
+        { textoOpcion: "Vanua Levu", respuestaCorrecta: false },
+        { textoOpcion: "Viti Levu", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué antigua cultura tuvo influencia significativa en Fiyi antes de la llegada de los europeos?",
+      opciones: [
+        { textoOpcion: "Inca", respuestaCorrecta: false },
+        { textoOpcion: "Polinesia", respuestaCorrecta: true },
+        { textoOpcion: "Maorí", respuestaCorrecta: false },
+        { textoOpcion: "Azteca", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué isla de Fiyi es un destino famoso para lunas de miel?",
+      opciones: [
+        { textoOpcion: "Viti Levu", respuestaCorrecta: false },
+        { textoOpcion: "Matangi", respuestaCorrecta: true },
+        { textoOpcion: "Vanua Levu", respuestaCorrecta: false },
+        { textoOpcion: "Taveuni", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿En qué año ganó Fiyi su independencia del Reino Unido?",
+      opciones: [
+        { textoOpcion: "1947", respuestaCorrecta: false },
+        { textoOpcion: "1970", respuestaCorrecta: true },
+        { textoOpcion: "1985", respuestaCorrecta: false },
+        { textoOpcion: "1955", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué isla es conocida por sus aguas termales naturales y mud pools en Fiyi?",
+      opciones: [
+        { textoOpcion: "Nanuya Lailai", respuestaCorrecta: false },
+        { textoOpcion: "Sabeto", respuestaCorrecta: true },
+        { textoOpcion: "Lautoka", respuestaCorrecta: false },
+        { textoOpcion: "Malolo", respuestaCorrecta: false },
+      ],
+    },
+    {
+      enunciado_pregunta: "¿Qué famoso explorador europeo fue el primero en llegar a Fiyi?",
+      opciones: [
+        { textoOpcion: "James Cook", respuestaCorrecta: false },
+        { textoOpcion: "Abel Tasman", respuestaCorrecta: false },
+        { textoOpcion: "Willem Schouten", respuestaCorrecta: false },
+        { textoOpcion: "Abel Tasman", respuestaCorrecta: true },
+      ],
+    }
+  ]
+  
+    
+    
+      
+      
+      
+      function getRandomInt(min, max) {
+        min = Math.ceil(min); // Redondea hacia arriba para asegurarse de que se incluya el mínimo
+        max = Math.floor(max); // Redondea hacia abajo para asegurarse de que se incluya el máximo
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+      
+      function seleccionarPreguntasAleatorias() {
+        let preguntaSeleccionadas = [];
+        let selecciones = [];
+      
+        for (let i = 0; i < 5; i++) {
+          let indice = getRandomInt(0, preguntas.length - 1);
+          console.log(indice);
+      
+          if (selecciones.includes(indice)) {
+            while (true) {
+              indice = getRandomInt(0, preguntas.length - 1);
+              if (!selecciones.includes(indice)) {
+                break;
+              }
+            }
+          }
+      
+          selecciones.push(indice);
+      
+          preguntaSeleccionadas.push(preguntas[indice]);
+        }
+      
+        return preguntaSeleccionadas;
+      }
+      
+      export { seleccionarPreguntasAleatorias };
+      
